387e4ac53194fda4630a5c265ef531b91dd5f8d4
Nethum Nimsara <nethum.nimsara294@gmail.com>
2026-03-25 22:07:23 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Health check
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
