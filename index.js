99487d32a1fcb0dda66173d4a2d1b9550472510c
Nethum Nimsara <nethum.nimsara294@gmail.com>
2026-03-27 21:45:19 +0530

    Update cors for production

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'https://nimsara294.github.io'
+    ]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
